f1026ccb3876df8ad19ea1db5e00d0a03f02ab85
デニス-さん <76629978+dcode-denpa@users.noreply.github.com>
2021-11-14 17:40:17 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 pkg clean
-pkg install figlet
-pkg install wget
-pkg install ffmpeg libwebp
-pkg install nodejs
-pkg install tesseract
+pkg install figlet -y
+pkg install wget -y
+pkg install ffmpeg libwebp -y
+pkg install nodejs -y
+pkg install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 clear
 npm start
